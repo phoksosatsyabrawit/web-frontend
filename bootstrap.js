b14623c859dbb50caf536792dcb77db62f8eba3c
phoksosatsyabrawit <phoksosatsyabrawit@gmail.com>
2025-03-07 21:21:31 +0700

    Add files via upload

---
+++ bootstrap.js
@@ -0,0 +1,22 @@
+function showresult(){
+    // alert("hello world");
+    //declare variable get from bootstrap
+    var empname = document.getElementById("name").value.trim();
+    var ememail = document.getElementById("email").value.trim();
+    var emsalary = document.getElementById("salary").value.trim();
+    var emdate = document.getElementById("hiredate").value.trim();
+    var fmcurrency = new Intl.NumberFormat("en-US",{
+        style:"currency",
+        currency:"USD"
+    });
+    var salarycurrency = fmcurrency.format(emsalary);
+
+    if(empname && ememail && emsalary && emdate !=null){
+        alert("1.My FullName is: "+empname+"\n"+
+              "2.My Email is: "+ememail+"\n"+
+              "3.My Salary is: "+salarycurrency+"\n"+
+              "4.Hire Date is: "+emdate);
+    }else{
+        alert("Please fill the information.");
+    }
+}
